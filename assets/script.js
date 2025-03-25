16be9236933f34fef5680b566db75cd7998dc15b
thomasctn <thomascoutant.2005@gmail.com>
2025-03-25 17:33:26 +0100

    ajout liste sous codes + cv

--- assets/script.js
+++ assets/script.js
@@ -15,21 +15,3 @@ function toggleMenu() {
     }
 }
 
-// Fonction pour ouvrir/fermer le sous-menu "Mes codes"
-document.getElementById('mesCodesLink').addEventListener('click', function(event) {
-    event.preventDefault(); // Empêche le lien de se comporter de manière classique
-
-    var dropdown = document.getElementById('mesCodesDropdown');
-    dropdown.classList.toggle('show'); // Bascule l'affichage du sous-menu
-});
-
-// Ferme le sous-menu si on clique en dehors de celui-ci
-document.addEventListener('click', function(event) {
-    var dropdown = document.getElementById('mesCodesDropdown');
-    var mesCodesLink = document.getElementById('mesCodesLink');
-
-    // Si le clic n'est pas sur le lien "Mes codes" ou sur le sous-menu
-    if (!mesCodesLink.contains(event.target) && !dropdown.contains(event.target)) {
-        dropdown.classList.remove('show'); // Ferme le sous-menu
-    }
-});
